2df128b9825ad44a28999327c858b4ea21ea5b38
IremKarakoyun <karakoyunirem24@gmail.com>
2024-10-21 15:50:10 +0200

    added security group and referenced it in ec2 resource with vpc_security_group_ids

--- Class2/main.tf
+++ Class2/main.tf
@@ -22,12 +22,43 @@ resource "aws_key_pair" "apr2024" {
 
 
 
+
+resource "aws_security_group" "terraform_class" {
+  name        = "terraform_class"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.terraform_class.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.terraform_class.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+
+
+
+
+
+
+
 resource "aws_instance" "web" {
   count         = 5
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   availability_zone = "us-east-1a"
   key_name = aws_key_pair.apr2024.key_name
+  vpc_security_group_ids = [ 
+    aws_security_group.terraform_class.id
+   ]
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
