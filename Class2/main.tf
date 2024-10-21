15caab3b4b773ad98012e1d717f047393b57b689
IremKarakoyun <karakoyunirem24@gmail.com>
2024-10-21 15:57:29 +0200

    added another inbound rule, http/80

--- Class2/main.tf
+++ Class2/main.tf
@@ -28,13 +28,22 @@ resource "aws_security_group" "terraform_class" {
   description = "Allow TLS inbound traffic and all outbound traffic"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_443" {
   security_group_id = aws_security_group.terraform_class.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_80" {
+  security_group_id = aws_security_group.terraform_class.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.terraform_class.id
