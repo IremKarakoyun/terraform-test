c602cafc9faf8eefbb6b89a4e22cdac288053823
IremKarakoyun <karakoyunirem24@gmail.com>
2024-10-21 17:17:14 +0200

    added security group/ssh

--- Class2/main.tf
+++ Class2/main.tf
@@ -44,6 +44,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_80" {
   to_port           = 80
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_22" {
+  security_group_id = aws_security_group.terraform_class.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.terraform_class.id
