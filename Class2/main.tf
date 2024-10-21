e27c733f788d4e8545807db369c6a1f5514db8d5
IremKarakoyun <karakoyunirem24@gmail.com>
2024-10-21 14:41:30 +0200

    added user data (installs apache)

--- Class2/main.tf
+++ Class2/main.tf
@@ -19,6 +19,14 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   availability_zone = "us-east-1"
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install apache2 -y
+              systemctl start apache2
+              systemctl enable apache2
+              echo "<h1>Welcome to Terraform Class 2</h1>" > /var/www/html/index.html
+              EOF
 
   tags = {
     Name = "HelloWorld"
