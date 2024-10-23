4349de55fc7919152b8b207a9fb37545be12ee29
IremKarakoyun <karakoyunirem24@gmail.com>
2024-10-23 14:07:02 +0200

    added more value info to variable.tf

--- advanced Terraform/variable.tf
+++ advanced Terraform/variable.tf
@@ -1,5 +1,17 @@
-variable "region" {}
+variable "region" {
+  description = "The AWS region to deploy the infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
 
-variable "key_name" {}
+variable "key_name" {
+  description = "The name of the key pair to use for SSH access"
+  type        = string
+  default     = "advanced-terraform"
+}
 
-variable "cidr_block" {}
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = ""
+}
